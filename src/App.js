ed9847990e0685556c27ce7284dff72135044932
Carola <Carola@MacBook-Pro-de-Santor.local>
2018-07-18 19:25:18 -0300

    cards individuales

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Products from './browser/components/products'
+import SingleProduct from './browser/containers/SingleProduct'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Products />
+        <SingleProduct />
       </div>
     );
   }
