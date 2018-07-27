a4d28f8e6fced62631a72d231225d699e9fad987
Carola <Carola@MacBook-Pro-de-Santor.local>
2018-07-27 16:55:15 -0300

    footer actualizado

--- src/browser/components/CardProduct.js
+++ src/browser/components/CardProduct.js
@@ -22,10 +22,6 @@ export default function (props) {
                         <div className={'oferta'}>
                             <p className={'titulo'}>OFERTA DEL DIA</p>
                         </div>
-
-                        <button onClick={() => handleClickCruz(card.id)} className={"ui-item_bookmark2"}>
-                            <span className={'cruz'} />
-                        </button>
                         {(!bookmark[card.id])
                             ? <button onClick={() => handleClick(card.id)} className={"ui-item_bookmark2"}><svg className={"ui-item_bookmark_icon2"} viewBox="0 0 36 32"><path d="M30.4 16q1.5-1.3 2-2.6t.6-3q0-1.4-.7-3T30.6 5q-1.4-1.2-2.4-1.6T25.8 3q-1.5 0-3 .6t-2.6 2l-2 2-2.3-2q-1.8-1.4-3-2T10.2 3t-2.6.4T5.3 5q-1 .7-1.6 2.4t-.7 3q0 1.4.6 3T5.4 16L18 28l12.4-12zM0 10.5q0-1.7.8-4t2.6-3.8Q5 1.2 6.7.7t3.6-.7q2 0 3.8.8t4 2.7q2-2 4-2.7t4-.8 3.4.6 3.3 2Q34.3 4 35 6.3t1 4-.6 4-3 4L18 32 3.4 18.2Q1 16 .4 13.7T0 10.4z"></path></svg>
                             </button>
@@ -55,4 +51,3 @@ export default function (props) {
     )
 }
 
-//<img src={arrow} alt='arr' style={arrowStyle} />
--- src/browser/containers/SingleProduct.js
+++ src/browser/containers/SingleProduct.js
@@ -8,25 +8,15 @@ export default class SingleProduct extends React.Component {
         super(props);
         this.state = {
             cards: [],
-            cardsRemove: {},
             bookmark: {}
         }
         this.handleClick = this.handleClick.bind(this)
-        this.handleClickCruz = this.handleClickCruz.bind(this)
+
     }
     handleClick = id =>
         this.setState(({ bookmark }) => ({
             bookmark: { ...bookmark, [id]: !bookmark[id] }
         }))
-
-    handleClickCruz = id => {
-        const index = this.state.cards.findIndex((card) => card.id === id)
-        const newCards = this.state.cards
-        newCards[index].show = false
-        this.setState({
-            cards: newCards
-        })
-    }
 
     componentDidMount() {
 
@@ -36,7 +26,7 @@ export default class SingleProduct extends React.Component {
             }).then(result => {
                 result.forEach((element, index) => {
                     return axios.get('https://api.mercadolibre.com/items/' + element.id).then(result => {
-                        result.data.show = true
+
                         this.setState(({ cards }) => ({
                             cards: [...cards, result.data],
                         })
@@ -50,11 +40,11 @@ export default class SingleProduct extends React.Component {
     }
 
     render() {
-        const filteredCards = this.state.cards.filter((card) => card.show)
+
         return (
             <div>
-                <CardProduct handleClick={this.handleClick} bookmark={this.state.bookmark} cards={filteredCards}
-                    handleClickCruz={this.handleClickCruz} cardsRemove={this.state.cardsRemove}
+                <CardProduct handleClick={this.handleClick} bookmark={this.state.bookmark} cards={this.state.cards}
+
                 />
             </div>
         )
