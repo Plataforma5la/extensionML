86373946e6f88c4ae1d2cc1a63d0fe22595764d7
Carola <Carola@MacBook-Pro-de-Santor.local>
2018-07-27 16:37:25 -0300

    actualizado

--- src/browser/containers/SingleProduct.js
+++ src/browser/containers/SingleProduct.js
@@ -8,14 +8,26 @@ export default class SingleProduct extends React.Component {
         super(props);
         this.state = {
             cards: [],
+            cardsRemove: {},
             bookmark: {}
         }
         this.handleClick = this.handleClick.bind(this)
+        this.handleClickCruz = this.handleClickCruz.bind(this)
     }
     handleClick = id =>
         this.setState(({ bookmark }) => ({
             bookmark: { ...bookmark, [id]: !bookmark[id] }
         }))
+
+    handleClickCruz = id => {
+        const index = this.state.cards.findIndex((card) => card.id === id)
+        const newCards = this.state.cards
+        newCards[index].show = false
+        this.setState({
+            cards: newCards
+        })
+    }
+
     componentDidMount() {
 
         axios.get('https://api.mercadolibre.com/sites/MLM/search?category=MLM1051&limit=3')
@@ -24,7 +36,7 @@ export default class SingleProduct extends React.Component {
             }).then(result => {
                 result.forEach((element, index) => {
                     return axios.get('https://api.mercadolibre.com/items/' + element.id).then(result => {
-
+                        result.data.show = true
                         this.setState(({ cards }) => ({
                             cards: [...cards, result.data],
                         })
@@ -38,10 +50,12 @@ export default class SingleProduct extends React.Component {
     }
 
     render() {
-
+        const filteredCards = this.state.cards.filter((card) => card.show)
         return (
             <div>
-                <CardProduct handleClick={this.handleClick} bookmark={this.state.bookmark} cards={this.state.cards} />
+                <CardProduct handleClick={this.handleClick} bookmark={this.state.bookmark} cards={filteredCards}
+                    handleClickCruz={this.handleClickCruz} cardsRemove={this.state.cardsRemove}
+                />
             </div>
         )
     }
