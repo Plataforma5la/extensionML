fefa757b59043e1b2a936c58925c882216717939
Carola <Carola@MacBook-Pro-de-Santor.local>
2018-08-06 16:21:00 -0300

    banner actualizado

--- src/browser/components/CardBanner.js
+++ src/browser/components/CardBanner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './cardBanner.css';
-import imagen from '../imagenes/imagen4.png'
+import imagen from '../imagenes/imagen3.jpg'
 
 
 
@@ -9,15 +9,13 @@ export default function () {
     return (
         <div className={"CardBanner"} >
 
-
             <a href={'https://ofertas.mercadolibre.com.mx/espacios-de-diseno#DEAL_ID=MLM107&S=homedesktop&V=5&L=espaciosdediseno&c_id=/home/exhibitors-carousel/element&c_campaign=http://ofertas.mercadolibre.com.mx/espacios-de-diseno#DEAL_ID=MLM107&S=homedesktop&V=5&L=espaciosdediseno&c_element_order=4'} target={"_blank"} >
 
                 <img id={'imgB'} src={imagen} alt='img' />
 
             </a>
 
-
-        </div>
+        </div >
 
     )
 }
