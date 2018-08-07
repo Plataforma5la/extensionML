0e62f1e30ee3cafeffd5f44d718c91c7733b532a
Manuel <mgrondona@gmail.com>
2018-08-07 17:38:42 -0300

    corrección

--- server/routes.js
+++ server/routes.js
@@ -87,8 +87,8 @@ router.get('/singleproducts', (req, res) => {
             return resultado
         })
         .then(resultado => res.json(resultado))
-        .catch(e => {
-            console.log(e);
+        .catch(err => {
+            console.log(err);
         })
 
 })
