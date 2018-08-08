2ecedd78d5f02d41fc4df6ff16cd7ad83041119b
Manuel <mgrondona@gmail.com>
2018-08-08 18:13:36 -0300

    unprotect config.js

--- src/config.js
+++ src/config.js
@@ -1,52 +1,52 @@
 
-// const config = {
-//     //Autenticación//
-//     clientId: '4678202253401617', //client ID app Mexico para estrategia passport/routes.js
-//     clientSecret: 'kDMjYvZfecqWi4MBoQsrVQGtJmQl9t0G', //client secret app México para estrategia passport/routes.js
-//     urlRedirect: 'http://www.mercadolibre.com.mx', //URL redirect para el callback de Oauth Passport y url para link del header/routes.js , HeaderInput.js y HeaderContainer.js
+const config = {
+    //Autenticación//
+    clientId: '4678202253401617', //client ID app Mexico para estrategia passport/routes.js
+    clientSecret: 'kDMjYvZfecqWi4MBoQsrVQGtJmQl9t0G', //client secret app México para estrategia passport/routes.js
+    urlRedirect: 'http://www.mercadolibre.com.mx', //URL redirect para el callback de Oauth Passport y url para link del header/routes.js , HeaderInput.js y HeaderContainer.js
 
-//     //Header
-//     search: 'http://listado.mercadolibre.com.mx/', //URL redirección search input / HeaderContainer.js
+    //Header
+    search: 'http://listado.mercadolibre.com.mx/', //URL redirección search input / HeaderContainer.js
 
-//     //Rutas Products//
-//     site: 'MLM', //site para rutas backend/routes.js
+    //Rutas Products//
+    site: 'MLM', //site para rutas backend/routes.js
 
-//     //Rutas Products para Primer Card//
-//     cat1CardDeals: 'MLM3813', //Categoría accesorios para celulares para primer producto CardDeals./routes.js
-//     cat2CardDeals: 'MLM3813', //Categoría accesorios para celulares para segundo producto CardDeals./routes.js
-//     cat3CardDeals: 'MLM1574', //Categoría Hogar, Muebles y Jardín para tercer producto CardDeals./routes.js
-//     priceRangeCardDealsCat1: '549-650', //Rango de precios para los productos de CardDeals/routes.js
-//     priceRangeCardDealsCat2: '549-650', //Rango de precios para los productos de CardDeals/routes.js
-//     priceRangeCardDealsCat3: '549-650', //Rango de precios para los productos de CardDeals/routes.js
-//     queryCardDealsCat1: '', //Query para primer categoría CardDeals (poner más palabras separadas por comas)/routes.js
-//     queryCardDealsCat2: 'audifonos, android', //Query para segunda categoría CardDeals (poner más palabras separadas por comas)/routes.js
-//     queryCardDealsCat3: 'antiadherente', //Query para tercer categoría CardDeals (poner más palabras separadas por comas)/routes.js
-//     shippingCostCardDealsCat1: 'free', //Envío gratis para primer categoría de CardDeals/routes.js
+    //Rutas Products para Primer Card//
+    cat1CardDeals: 'MLM3813', //Categoría accesorios para celulares para primer producto CardDeals./routes.js
+    cat2CardDeals: 'MLM3813', //Categoría accesorios para celulares para segundo producto CardDeals./routes.js
+    cat3CardDeals: 'MLM1574', //Categoría Hogar, Muebles y Jardín para tercer producto CardDeals./routes.js
+    priceRangeCardDealsCat1: '549-650', //Rango de precios para los productos de CardDeals/routes.js
+    priceRangeCardDealsCat2: '549-650', //Rango de precios para los productos de CardDeals/routes.js
+    priceRangeCardDealsCat3: '549-650', //Rango de precios para los productos de CardDeals/routes.js
+    queryCardDealsCat1: '', //Query para primer categoría CardDeals (poner más palabras separadas por comas)/routes.js
+    queryCardDealsCat2: 'audifonos, android', //Query para segunda categoría CardDeals (poner más palabras separadas por comas)/routes.js
+    queryCardDealsCat3: 'antiadherente', //Query para tercer categoría CardDeals (poner más palabras separadas por comas)/routes.js
+    shippingCostCardDealsCat1: 'free', //Envío gratis para primer categoría de CardDeals/routes.js
 
-//     //Rutas Products para Single Product Cards//
-//     cat1SingleCard: 'MLM1000', //Categoría Electrónica, Audio y Video para primer producto Single Card./routes.js
-//     cat2SingleCard: 'MLM1403', //Categoría Alimentos y Bebidas para segundo producto Single Card./routes.js
-//     qSingleCard2: 'pack,cerveza', //query para traer pack de cerveza de Alimentos y Bebidas Single Card./routes.js
-//     discountSingleCard1: '30-100',//query para traer productos con un rango de descuentos de Electrónica, Audio y Video Single Card./routes.js
+    //Rutas Products para Single Product Cards//
+    cat1SingleCard: 'MLM1000', //Categoría Electrónica, Audio y Video para primer producto Single Card./routes.js
+    cat2SingleCard: 'MLM1403', //Categoría Alimentos y Bebidas para segundo producto Single Card./routes.js
+    qSingleCard2: 'pack,cerveza', //query para traer pack de cerveza de Alimentos y Bebidas Single Card./routes.js
+    discountSingleCard1: '30-100',//query para traer productos con un rango de descuentos de Electrónica, Audio y Video Single Card./routes.js
 
-//     //Componente Banner//
-//     bannerUrl: 'https://ofertas.mercadolibre.com.mx/espacios-de-diseno#DEAL_ID=MLM107&S=homedesktop&V=5&L=espaciosdediseno&c_id=/home/exhibitors-carousel/element&c_campaign=http://ofertas.mercadolibre.com.mx/espacios-de-diseno#DEAL_ID=MLM107&S=homedesktop&V=5&L=espaciosdediseno&c_element_order=3', //url link del componente banner/ cardBanner.js
+    //Componente Banner//
+    bannerUrl: 'https://ofertas.mercadolibre.com.mx/espacios-de-diseno#DEAL_ID=MLM107&S=homedesktop&V=5&L=espaciosdediseno&c_id=/home/exhibitors-carousel/element&c_campaign=http://ofertas.mercadolibre.com.mx/espacios-de-diseno#DEAL_ID=MLM107&S=homedesktop&V=5&L=espaciosdediseno&c_element_order=3', //url link del componente banner/ cardBanner.js
 
-//     //URL Ofertas Semana//
-//     linkOfertasSemana: 'https://ofertas.mercadolibre.com.mx/ofertas-de-la-semana', //url botón "Ver todas las ofertas" en cardDeals y "Ofertas de la Semana" de Footer/cardDeals.js y Footer.js
+    //URL Ofertas Semana//
+    linkOfertasSemana: 'https://ofertas.mercadolibre.com.mx/ofertas-de-la-semana', //url botón "Ver todas las ofertas" en cardDeals y "Ofertas de la Semana" de Footer/cardDeals.js y Footer.js
 
-//     //URLs para el footer//
-//     urlMiCuenta: 'https://myaccount.mercadolibre.com.mx/', //url mi cuenta para footer/Footer.js
-//     urlTusCompras: 'https://myaccount.mercadolibre.com.mx/purchases/list', //url tus compras para footer/Footer.js
-//     urlTuHistorial: 'https://www.mercadolibre.com.mx/gz/home/navigation', //url tu historial para footer/Footer.js
-//     urlFavoritos: 'https://myaccount.mercadolibre.com.mx/bookmarks/list', //url favoritos para footer/Footer.js
-//     urlTiendasOficiales: 'https://www.mercadolibre.com.mx/tiendas-oficiales', //url tiendas oficiales para footer/Footer.js
-//     urlCategorias: 'https://home.mercadolibre.com.mx/categories', //url categorías para footer/Footer.js
-//     urlMercadoPuntos: 'https://www.mercadolibre.com.mx/mercadopuntos', //url mercado puntos para footer/Footer.js
-//     urlAyuda: 'https://www.mercadolibre.com.mx/ayuda', //url ayuda para footer/Footer.js
-//     urlVender: 'https://vender.mercadolibre.com.mx/',//url vender para footer/Footer.js
-//     copyright: 'Copyright © 1999-2018 DeRemate.com de México S. de R.L. de C.V.' //copyright para footer/Footer.js
+    //URLs para el footer//
+    urlMiCuenta: 'https://myaccount.mercadolibre.com.mx/', //url mi cuenta para footer/Footer.js
+    urlTusCompras: 'https://myaccount.mercadolibre.com.mx/purchases/list', //url tus compras para footer/Footer.js
+    urlTuHistorial: 'https://www.mercadolibre.com.mx/gz/home/navigation', //url tu historial para footer/Footer.js
+    urlFavoritos: 'https://myaccount.mercadolibre.com.mx/bookmarks/list', //url favoritos para footer/Footer.js
+    urlTiendasOficiales: 'https://www.mercadolibre.com.mx/tiendas-oficiales', //url tiendas oficiales para footer/Footer.js
+    urlCategorias: 'https://home.mercadolibre.com.mx/categories', //url categorías para footer/Footer.js
+    urlMercadoPuntos: 'https://www.mercadolibre.com.mx/mercadopuntos', //url mercado puntos para footer/Footer.js
+    urlAyuda: 'https://www.mercadolibre.com.mx/ayuda', //url ayuda para footer/Footer.js
+    urlVender: 'https://vender.mercadolibre.com.mx/',//url vender para footer/Footer.js
+    copyright: 'Copyright © 1999-2018 DeRemate.com de México S. de R.L. de C.V.' //copyright para footer/Footer.js
 
-// }
+}
 
-// module.exports = config
+module.exports = config
