30c9df556563aa6253d92eca4ca6dd3e5daa6e23
Manuel <mgrondona@gmail.com>
2018-08-08 12:12:27 -0300

    ordenamiento config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,14 @@
 
 const config = {
+    //Autenticación//
     clientId: '6429131972786101', //client ID app Argentina para estrategia passport/routes.js
     clientSecret: 'iFuKIyLLhpbg99KyrsPCUqVSleiHYhcf', //client secret app Argentina para estrategia passport/routes.js
     urlRedirect: 'http://www.mercadolibre.com.ar', //URL redirect para el callback de Oauth Passport y url para link del header/routes.js y HeaderInput.js
+
+    //Rutas Products//
     site: 'MLA', //site para rutas backend/routes.js
+
+    //Rutas Products para Primer Card//
     cat1CardDeals: 'MLA1055', //Categoría celulares y telefonía para primer producto CardDeals./routes.js
     cat2CardDeals: 'MLA3502', //Categoría accesorios para celulares para segundo producto CardDeals./routes.js
     cat3CardDeals: 'MLA1574', //Categoría Hogar, Muebles y Jardín para tercer producto CardDeals./routes.js
@@ -14,15 +19,20 @@ const config = {
     queryCardDealsCat2: 'auriculares,bluetooth', //Query para segunda categoría CardDeals (poner más palabras separadas por comas)/routes.js
     queryCardDealsCat3: 'cocina,sarten', //Query para tercer categoría CardDeals (poner más palabras separadas por comas)/routes.js
     shippingCostCardDealsCat1: 'free', //Envío gratis para primer categoría de CardDeals/routes.js
+
+    //Rutas Products para Single Product Cards//
     cat1SingleCard: 'MLA1000', //Categoría Electrónica, Audio y Video para primer producto Single Card./routes.js
     cat2SingleCard: 'MLA1403', //Categoría Alimentos y Bebidas para segundo producto Single Card./routes.js
     qSingleCard2: 'packcerveza', //query para traer pack de cerveza de Alimentos y Bebidas Single Card./routes.js
     discountSingleCard1: '30-100',//query para traer productos con un rango de descuentos de Electrónica, Audio y Video Single Card./routes.js
-    //---------------------------------------------------------------------------------------------------------------------
+
+    //Componente Banner//
     bannerUrl: 'https://ofertas.mercadolibre.com.ar/especial-juguetes#DEAL_ID=mla813&S=homedesktop&V=3&L=especialjuguetes&c_id=/home/exhibitors-carousel/element&c_campaign=especial-juguetes&c_element_order=3', //url link del componente banner/ cardBanner.js
-    //---------------------------------------------------------------------------------------------------------------------
+
+    //URL Ofertas Semana//
     linkOfertasSemana: 'https://ofertas.mercadolibre.com.ar/ofertas-de-la-semana', //url botón "Ver todas las ofertas" en cardDeals y "Ofertas de la Semana" de Footer/cardDeals.js y Footer.js
-    //---------------------------------------------------------------------------------------------------------------------
+
+    //URLs para el footer//
     urlMiCuenta: 'https://myaccount.mercadolibre.com.ar', //url mi cuenta para footer/Footer.js
     urlTusCompras: 'https://myaccount.mercadolibre.com.ar/purchases/list/', //url tus compras para footer/Footer.js
     urlTuHistorial: 'https://www.mercadolibre.com.ar/gz/home/navigation', //url tu historial para footer/Footer.js
